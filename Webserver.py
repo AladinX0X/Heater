73ff289ed50c169cb1bccb299d02fc855cfc7fd6
AladinX0X <121769504+AladinX0X@users.noreply.github.com>
2024-06-06 19:05:09 +0200

    Update Webserver.py

--- Webserver.py
+++ Webserver.py
@@ -7,14 +7,15 @@ PORT = 8000
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
-            self.path = '/frontend/index.html'
+            self.path = '/index.html'
         elif self.path == '/app.js':
-            self.path = '/frontend/app.js'
+            self.path = '/app.js'
         elif self.path == '/styles.css':
-            self.path = '/frontend/styles.css'
+            self.path = '/styles.css'
         elif self.path == '/1920px-Fraunhofer-Gesellschaft_2009_logo.svg_.png':
-            self.path = '/frontend/1920px-Fraunhofer-Gesellschaft_2009_logo.svg_.png'
+            self.path = '/1920px-Fraunhofer-Gesellschaft_2009_logo.svg_.png'
         super().do_GET()
+
 
 def run_server():
     web_dir = os.path.join(os.path.dirname(__file__), 'frontend')
